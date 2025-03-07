58df3b206ac86e9e3b9613e7b725e4f76561efe1
Anton <125435667+anton206@users.noreply.github.com>
2025-03-07 11:18:28 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,64 @@
+<svelte:head>
+    <title>Zajn Jasnowidz</title>
+</svelte:head>
+
+<header>Zajn Jasnowidz</header>
+<div id="zod">
+    <span id="tit">Wybierz swój znak zodiaku</span>
+    <br />
+    <a class="zodiaki" style="margin-top:25px;" href="/zodiak/baran">
+        <img alt="" src="/tile001.png" /><br /><span class="tytzod">Baran</span
+        ><br />21Mar, - 20Kwiet.
+    </a>
+    <a class="zodiaki" style="margin-top:25px;" href="/zodiak/byk">
+        <img alt="" src="/tile010.png" /><br /><span class="tytzod">Byk</span
+        ><br />20Kwiet, - 20Maj
+    </a>
+    <a class="zodiaki" style="margin-top:25px;" href="/zodiak/bliznieta">
+        <img alt="" src="/tile005.png" /><br /><span class="tytzod"
+            >Bliźnięta</span
+        ><br />21Maj - 21Czer.
+    </a>
+    <a class="zodiaki" style="margin-top:25px;" href="/zodiak/rak">
+        <img alt="" src="/tile002.png" /><br /><span class="tytzod">Rak</span
+        ><br />21Czer. - 22Lip.
+    </a>
+    <a class="zodiaki" style="clear:both;" href="/zodiak/lew">
+        <img alt="" src="/tile006.png" /><br /><span class="tytzod">Lew</span
+        ><br />23Lip. - 23Sier.
+    </a>
+    <a class="zodiaki" href="/zodiak/panna">
+        <img alt="" src="/tile011.png" /><br /><span class="tytzod">Panna</span
+        ><br />23Sier. - 23Wrze.
+    </a>
+    <a class="zodiaki" href="/zodiak/waga">
+        <img alt="" src="/tile007.png" /><br /><span class="tytzod">Waga</span
+        ><br />23Wrze. - 23Paź.
+    </a>
+    <a class="zodiaki" href="/zodiak/skorpion">
+        <img alt="" src="/tile004.png" /><br /><span class="tytzod"
+            >Skorpion</span
+        ><br />23Paź. - 22List.
+    </a>
+    <a class="zodiaki" style="clear:both;" href="/zodiak/strzelec">
+        <img alt="" src="/tile009.png" /><br /><span class="tytzod"
+            >Strzelec</span
+        ><br />22List. - 21Gru.
+    </a>
+    <a class="zodiaki" href="/zodiak/koziorozec">
+        <img alt="" src="/tile003.png" /><br /><span class="tytzod"
+            >Koziorożec</span
+        ><br />21Gru. - 20Sty.
+    </a>
+    <a class="zodiaki" href="/zodiak/wodnik">
+        <img alt="" src="/tile000.png" /><br /><span class="tytzod">Wodnik</span
+        ><br />20Sty. - 19Lut.
+    </a>
+    <a class="zodiaki" href="/zodiak/ryby">
+        <img alt="" src="/tile008.png" /><br /><span class="tytzod">Ryby</span
+        ><br />19Lut. - 20Mar.
+    </a>
+</div>
+<footer style="clear:both;">
+    <br /><br />&copy; Copyright
+</footer>
