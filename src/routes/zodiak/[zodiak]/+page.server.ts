d24ac896d23ca5c92a6f488820f45dd93a41bccb
Anton <125435667+anton206@users.noreply.github.com>
2025-03-08 16:20:24 +0100

    ai

--- src/routes/zodiak/[zodiak]/+page.server.ts
+++ src/routes/zodiak/[zodiak]/+page.server.ts
@@ -1,6 +1,14 @@
 import type { Actions } from "./$types";
 import nodemailer from "nodemailer";
-import { SMTP_PASSWORD } from "$env/static/private";
+import { SMTP_PASSWORD, OPENAI_API_KEY } from "$env/static/private";
+import OpenAI from 'openai';
+import { RateLimiter } from 'sveltekit-rate-limiter/server';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+const client = new OpenAI({
+    apiKey: OPENAI_API_KEY,
+});
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -12,19 +20,40 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+
+const limiter = new RateLimiter({
+    IP: [2, 'm'],
+    IPUA: [1, 'm'],
+});
+
+const zodiaki = ["baran", "byk", "bliznieta", "rak", "lew", "panna", "waga", "skorpion", "strzelec", "koziorozec", "wodnik", "ryby"];
+
+export const load: PageServerLoad = async (event) => {
+    await limiter.cookieLimiter?.preflight(event);
+};
+
 export const actions: Actions = {
-    default: async ({ params, request }) => {
-        const data = await request.formData();
-        const mail = data.get('mail');
-        const imie = data.get('imie');
-        const nazwisko = data.get('nazwisko');
-        const zodiak = params.zodiak;
+    default: async (event) => {
+        if (await limiter.isLimited(event)) throw error(429);
+
+        const data = await event.request.formData();
+        const mail = data.get('mail') as string;
+        const imie = data.get('imie') as string;
+        const nazwisko = data.get('nazwisko') as string;
+        const zodiak = event.params.zodiak;
+
+        if (!zodiaki.includes(zodiak)) throw Error("sus zodiak");
+
+        const resp = await client.chat.completions.create({
+            messages: [{ role: 'system', content: `Jesteś Zajn Jasnowidz. Wygeneruj horoskop dla "${imie} ${nazwisko}" ze znakiem zodiaku ${zodiak}. Użyj HTML zamiast Markdown. Nie używaj tagu style.` }],
+            model: 'gpt-4o',
+        });
 
         await transporter.sendMail({
             from: '"Zajn Jasnowidz" <realjerzystarak@gmail.com>',
             to: mail,
-            subject: "TWÓJ HOROSKOP",
-            text: `${imie} ${nazwisko} ${zodiak}`,
+            subject: "Twój Horoskop",
+            html: resp.choices[0].message.content!,
         });
     }
 };
